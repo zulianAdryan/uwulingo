16cf633983249ba61c5ea8dd93fc5c4a8596cde8
zulianAdryan <zulian.adryan@gmail.com>
2025-02-04 12:09:24 +0700

    feat: courses page

---
+++ app/(main)/courses/page.tsx
@@ -0,0 +1,21 @@
+import { getCourses, getUserProgress } from "@/db/queries";
+import { List } from "./list";
+
+const Page = async ({}) => {
+  const courseData = getCourses();
+  const userProgressData = getUserProgress();
+
+  const [courses, userProgress] = await Promise.all([
+    courseData,
+    userProgressData,
+  ]);
+
+  return (
+    <div className="h-full max-w-[912px] px-3 mx-auto">
+      <h1 className="text-2xl font-bold text-neutral-700">Language Courses</h1>
+      <List courses={courses} activeCourseId={userProgress?.activeCourseId} />
+    </div>
+  );
+};
+
+export default Page;
